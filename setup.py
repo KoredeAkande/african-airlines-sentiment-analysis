93c284fc81061a04b5155429753cb0dffdec640c
Korede Akande <55578569+KoredeAkande@users.noreply.github.com>
2021-10-04 06:47:00 +0200

    Uploaded project files
    Uploaded local files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An Aspect-Based Sentiment Analysis (ABSA) of the top 5 airlines globally',
+    author='Oluwakorede Akande',
+    license='MIT',
+)
